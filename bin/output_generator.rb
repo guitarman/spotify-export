d675658211dfa632a3f2ee5706cba824ec60033a
guitarman <guitarman@guitarman.sk>
2014-07-16 23:31:53 +0200

    file operations

--- bin/output_generator.rb
+++ bin/output_generator.rb
@@ -7,28 +7,43 @@ class OutputGenerator
     @progressbar = progressbar
   end
 
+  def open_file
+    @file = File.new(@filename, 'w:utf-8')
+  end
+
+  def close_file
+    @file.close
+  end
+
   def generate
-    @filename = "output.txt" unless filename
+    @filename = 'output.txt' unless filename
+    open_file
     @tracks.each_with_index do |track, count|
       # Sanity check
       unless track.nil?
-        generate_track_record(track)
+        @file.write generate_track_record(track)
         @progressbar.increment
       end
     end
+    close_file
   end
 end
 
 class M3uGenerator < OutputGenerator
 
+  def open_file
+    super
+    @file.write "#EXTM3U\n"
+  end
+
   def generate_track_record(track)
-    puts track.m3u_record
+    track.m3u_record
   end
 end
 
 class PlainTextGenerator < OutputGenerator
 
   def generate_track_record(track)
-    puts "#{ track.name } -- #{ track.artist } -- #{ track.album }\n"
+    "#{ track.name } -- #{ track.artist } -- #{ track.album }\n"
   end
 end
